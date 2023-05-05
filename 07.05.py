32df5d4d6bfd2fd72cacdff931eeb164f5c3000b
Hrechanyuk Anastasiia <anastasiya.grk@gmail.com>
2023-05-05 18:09:23 +0300

    №2

---
+++ 07.05.py
@@ -0,0 +1,8 @@
+#2
+def max_lenth(*args):
+    max_len = 0
+    for arg in args:
+        if len(arg) > max_len:
+            max_len = len(arg)
+    return print("Максимальна кількість символів у рядку:", max_len)
+max_lenth("good", "like", "instagram", "viber", "telegram")
